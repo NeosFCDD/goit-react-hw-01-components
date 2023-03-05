1bd39d51d0f8cbf43e0d6d4afee15400ac10462a
Volodymyr Hudzula <110738058+NeosFCDD@users.noreply.github.com>
2023-03-05 22:09:16 +0200

    statis

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import Profile from './Profile/Profile';
 import user from '../backend/user.json';
+import Statistics from './Statistics/Statistics';
+import data from '../backend/data.json';
+
 
 export const App = () => {
   return (
@@ -11,6 +14,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
